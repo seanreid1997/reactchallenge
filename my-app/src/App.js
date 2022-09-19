8a31cb0651d3e8cc6d9ab2952aafc563c418caaf
seanreid1997 <reid.sean14@yahoo.com>
2022-09-19 03:04:49 +0000

    Had to refer to source code to complete challenge 4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
+import NavBarForm from './components/NavBarForm';
 
 function App() {
   return (
     <div className={css.App}>
-      <Sidebar />
+      <NavBarForm />
     </div>
   );
 }
