16cf17629ac64e7b206ee7175bbc8b0751dc01f8
seanreid1997 <reid.sean14@yahoo.com>
2022-09-22 22:17:29 +0000

    Complete challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
+import Content from './components/Content';
 
 function App() {
   return (
     <div className={css.App}>
-      <NavBarForm />
+      <Content />
     </div>
   );
 }
---
+++ my-app/src/components/Content.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import css from "./css/Content.module.css";
+import {savedPosts} from "../posts.json";
+import PostItem from "./PostItem";
+
+export class Content extends Component {
+    constructor(props) {
+        super(props)
+    }
+    
+    render() {
+        return (
+            <div className={css.Content}>
+                
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                </div>
+
+                <div className={css.SearchResults}>
+
+                        {
+                    savedPosts.map((post)=>{
+                            return <div className={css.SearchItem} key={post.title}>
+                                <p>{post.title}</p>
+                                <p>{post.name}</p>
+                                <img src={post.image} alt="random"/>
+                                <p>{post.description}</p>
+                                </div>
+                        })
+                    }
+
+                    <PostItem savedPosts={savedPosts} />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Content
